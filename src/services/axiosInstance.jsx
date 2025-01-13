773aa2a3cc843d0051e08e19ddc53a354f48de85
Kevin <kevinjamesbiju2000@gmail.com>
2025-01-13 23:15:27 +0530

    changed url

--- src/services/axiosInstance.jsx
+++ src/services/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://inventory-management-system-backend-gys0.onrender.com",
   withCredentials: true,
 });
 
