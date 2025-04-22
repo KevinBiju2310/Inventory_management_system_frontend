75b6684500c4ffc395e12612864ff0fea5004d5f
Kevin <kevinjamesbiju2000@gmail.com>
2025-04-22 10:47:25 +0530

    made changes for hosting

--- src/services/axiosInstance.jsx
+++ src/services/axiosInstance.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://inventory-management-system-backend-gys0.onrender.com",
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
